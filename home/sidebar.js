e33d9c649e803b60c088b4b7feac3ac1fdd220ef
TheMiranha <lxbrmiranda@gmail.com>
2022-05-30 01:50:02 -0300

    first commit

---
+++ home/sidebar.js
@@ -0,0 +1,10 @@
+import HeadsetIcon from '@mui/icons-material/Headset';
+
+const Item = () => {
+    return {
+        title: "Spotify",
+        icon: <HeadsetIcon style={{marginRight: 10}}/>
+    }
+}
+
+export default { Item }
